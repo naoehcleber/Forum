91758853ab2c281c793c98026a024900cc860f99
naoehcleber <joao.vr.fernandes@gmail.com>
2025-08-12 00:24:20 -0300

    Update: função handleLikeFunction atualizda

--- client/src/utils/Likes.js
+++ client/src/utils/Likes.js
@@ -2,7 +2,25 @@ import React, { useState, useEffect } from "react";
 
 const Likes = ({numberOfLikes, threadId}) => {
     const handleLikeFunction = () => {
-        alert("Post Curtido");
+        fetch("http://localhost:4000/api/thread/like", {
+            method: "POST",
+            body: JSON.stringify({
+                threadId,
+                userId: localStorage.getItem("_id"),
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            if(data.error_message){
+                alert(data.error_message);
+            }else{
+                alert(data.message);
+            }
+        })
+        .catch((err) => console.error(err));
     };
 
     return (
