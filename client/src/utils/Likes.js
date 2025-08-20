b000f85a32164f194f3b006860b30064f015a900
naoehcleber <joao.vr.fernandes@gmail.com>
2025-08-20 01:57:05 -0300

    Revert "Update: função handleLikeFunction atualizda"
    This reverts commit 91758853ab2c281c793c98026a024900cc860f99.
    
    Remoção do Docker + CouchDB por dificuldade de implementação

--- client/src/utils/Likes.js
+++ client/src/utils/Likes.js
@@ -2,25 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Likes = ({numberOfLikes, threadId}) => {
     const handleLikeFunction = () => {
-        fetch("http://localhost:4000/api/thread/like", {
-            method: "POST",
-            body: JSON.stringify({
-                threadId,
-                userId: localStorage.getItem("_id"),
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            if(data.error_message){
-                alert(data.error_message);
-            }else{
-                alert(data.message);
-            }
-        })
-        .catch((err) => console.error(err));
+        alert("Post Curtido");
     };
 
     return (
